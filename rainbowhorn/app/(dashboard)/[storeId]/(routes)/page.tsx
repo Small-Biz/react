bf7e95262dfaa4e0efdb7372274a62d9988efe5d
Ray Cheng <ray101682@gmail.com>
2023-07-14 22:32:16 +0100

    bug fix FC issue

--- rainbowhorn/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ rainbowhorn/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,39 @@
-const DashboardPage = () => {
+"use client"
+
+import axios from "axios";
+import { useState } from "react";
+
+interface DashboardPageProps{
+    params: {storeId: string}
+};
+
+const DashboardPage:React.FC<DashboardPageProps>=({
+    params
+}) => {
+
+    const [store,setStore] = useState(null);
+
+    if (!store){
+        axios.get('http://localhost:8090/admin/store')///'+params.storeId)
+        .then( function(response){
+            setStore(response.data.store);
+        })
+    }else{
+
+        return (
+            <div>
+                Active Store: {store['name']}
+            </div>
+        )
+
+    }
+
     return (
         <div>
-            This is a Dashboard
+            Loading...
         </div>
     )
+
 }
 
 export default DashboardPage;
