9ea21f93764f686e61e6b82e1227c7bd79b52c98
Ray Cheng <ray101682@gmail.com>
2023-07-15 09:25:40 +0100

    add setting form but with bug

---
+++ rainbowhorn/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs";
+import axios from "axios";
+import { redirect } from "next/navigation";
+import { SettingsForm } from "./components/settings-form";
+
+interface SettingsPageProps{
+    params:{
+        storeId:string;
+    }
+}
+
+const SettingPage:React.FC<SettingsPageProps>=async ({
+    params
+})=>{
+
+    const {userId}=auth();
+
+    if (!userId){
+        redirect('/sign-in');
+    }
+
+    const response= await axios.get('http://localhost:8090/admin/store');//+storeId);
+
+    if (!response){
+        redirect('/');
+    }
+
+    const store=response.data.store;
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <SettingsForm initialData={store}/>
+            </div>
+        </div>
+    )
+}
+
+export default SettingPage;
