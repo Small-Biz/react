99bb8577d55ae63f9f453cb11e520429cb87eb52
Ray Cheng <ray101682@gmail.com>
2023-07-19 23:14:26 +0100

    bug fix typing error

--- rainbowhorn/components/store-switcher.tsx
+++ rainbowhorn/components/store-switcher.tsx
@@ -40,8 +40,8 @@ export default function StoreSwitcher({
 
     return (
         <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger>
-                <Button 
+            <PopoverTrigger asChild>
+                <Button
                     variant="outline"
                     size="sm"
                     role="combobox"
@@ -51,6 +51,8 @@ export default function StoreSwitcher({
                     <StoreIcon className="mr-2 h-4 w-4"/>
                     {currentStore?.label}
                     <ChevronsUpDownIcon className="ml-auto h-4 w-4 shrink-0 opacity-50"/>
+                </Button>
+            </PopoverTrigger>
                     <PopoverContent className="w-[200px] p-0">
                         <Command>
                             <CommandList>
@@ -92,8 +94,6 @@ export default function StoreSwitcher({
                             </CommandList>
                         </Command>
                     </PopoverContent>
-                </Button>
-            </PopoverTrigger>
         </Popover>
     )
 }
