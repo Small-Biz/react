ead9a0f27e284a22d7c63a1791e7bccfd3bce85e
Ray Cheng <ray101682@gmail.com>
2023-07-26 21:49:32 +0100

    create confirm order with customer info

--- rainbowhorn-store/app/(routes)/checkout/components/checkout-form.tsx
+++ rainbowhorn-store/app/(routes)/checkout/components/checkout-form.tsx
@@ -1,16 +1,49 @@
 "use client";
 
 import Button from "@/components/ui/button";
-import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { MouseEventHandler, useState } from "react";
+import { AddressElement, LinkAuthenticationElement, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { MouseEventHandler, useEffect, useState } from "react";
 
 const CheckoutForm = () => {
 
     const stripe = useStripe();
     const elements = useElements();
 
-    const [errorMessage, setErrorMessage] = useState(null);
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     
+    useEffect(() => {
+        if (!stripe) {
+          return;
+        }
+    
+        const clientSecret = new URLSearchParams(window.location.search).get(
+          "payment_intent_client_secret"
+        );
+    
+        if (!clientSecret) {
+          return;
+        }
+    
+        stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+          switch (paymentIntent?.status) {
+            case "succeeded":
+              setMessage("Payment succeeded!");
+              break;
+            case "processing":
+              setMessage("Your payment is processing.");
+              break;
+            case "requires_payment_method":
+              setMessage("Your payment was not successful, please try again.");
+              break;
+            default:
+              setMessage("Something went wrong.");
+              break;
+          }
+        });
+      }, [stripe]);
+
     const onConfirmPayment: MouseEventHandler<HTMLButtonElement> = async (event)=>{
         // We don't want to let default form submission happen here,
         // which would refresh the page.
@@ -22,6 +55,8 @@ const CheckoutForm = () => {
           return;
         }
     
+        setIsLoading(true);
+
         const {error} = await stripe.confirmPayment({
           //`Elements` instance that was used to create the Payment Element
           elements,
@@ -30,22 +65,42 @@ const CheckoutForm = () => {
           },
         });
     
-        if (error) {
-          // This point will only be reached if there is an immediate error when
-          // confirming the payment. Show error to your customer (for example, payment
-          // details incomplete)
-          setErrorMessage(error.message);
-        } else {
-          // Your customer will be redirected to your `return_url`. For some payment
-          // methods like iDEAL, your customer will be redirected to an intermediate
-          // site first to authorize the payment, then redirected to the `return_url`.
-        }
+        if (error.type === "card_error" || error.type === "validation_error") {
+            setMessage(error.message);
+          } else {
+            setMessage("An unexpected error occurred.");
+          }
+
+        setIsLoading(false);
       };
+
+        const paymentElementOptions = {
+            layout: "tabs"
+        }
 
     return (
         <form>
+            <LinkAuthenticationElement
+                id="link-authentication-element"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <h3>Shipping</h3>
+            <AddressElement options={{
+                mode: 'shipping',
+                allowedCountries: ['GB','FR'],
+                fields: {
+                    phone: 'always',
+                },
+                validation: {
+                  phone: {
+                    required: 'always',
+                  },
+                },}} />
             <PaymentElement />
-            <Button onClick={onConfirmPayment} disabled={!stripe}>Submit</Button>
+            <Button onClick={onConfirmPayment} disabled={isLoading || !stripe || !elements}>Submit</Button>
+
+            {/* Show any error or success messages */}
+            {message && <div id="payment-message">{message}</div>}
         </form>
     )
 }
