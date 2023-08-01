668c78064e853fb2bfc139b4e3baa74461991c31
Ray Cheng <ray101682@gmail.com>
2023-08-01 23:35:02 +0100

    payment success page

--- rainbowhorn-store/app/(routes)/checkout/result/page.tsx
+++ rainbowhorn-store/app/(routes)/checkout/result/page.tsx
@@ -4,6 +4,8 @@ import qs from "query-string"
 import confirmPayment from "@/actions/confirm-payment";
 import useCart from "@/hooks/use-cart";
 import { useSearchParams } from "next/navigation";
+import Container from "@/components/ui/container";
+import Button from "@/components/ui/button";
 
 const CheckoutResultPage = () => {
 
@@ -22,9 +24,29 @@ const CheckoutResultPage = () => {
     removeAll();
 
     return (
-        <div>
-            Checkout Success 2
-        </div>
+        <Container>
+            <div className="p-4 sm:p-6 lg:p-8 overflow-hidden order-solid">
+                <div  className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+                    <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+                        <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+                            Payment success
+                        </div>
+                        <div>
+                            Your Order ID: {current['payment_intent']}
+                        </div>
+                        <span>
+                            Your order are in progress. If you want to check the order status, you could go to <a className="text-sky-600">Order Details</a> page. 
+                        </span>
+                        <div>
+                            <Button>
+                                Go Shopping
+                            </Button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </Container>
     )
 }
 
